dbf9d42bc934db410c48280586d779dddf0b5086
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2020-12-23 11:09:00 -0800

    read files

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const fs = require("fs")
+const http = require("http")
+const url = require("url")
+const slugify = require("slugify")
+
+//synchronous version is fine because this is only called once, when the server is started
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+)
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+)
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+const dataObj = JSON.parse(data)
