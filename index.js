48aa655e297e88a2f7e3244c8bb38f38d007d8c2
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2020-12-24 09:08:48 -0800

    starting on routing

--- index.js
+++ index.js
@@ -17,5 +17,52 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 )
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }))
+const replaceTemplate = require("./modules/replaceTemplate")
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true)
+  // /whatever?=stuff, query = stuff
+  // pathname = /whatever
+  const slug = slugs.indexOf(query)
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" })
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("")
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml)
+
+    res.end(output)
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" })
+    const product = dataObj[slug]
+    const output = replaceTemplate(tempProduct, product)
+
+    res.end(output)
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" })
+    res.end(data)
+
+    // 404
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    })
+    res.end("<h1>Page not found</h1>")
+  }
+})
+
+// port, localhost, callback
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000")
+})
