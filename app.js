c747c7b9c8d17706b8ef773e20b308043c682b51
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2021-01-17 13:14:10 -0800

    update data.json

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
 
+const inventoryController = require("./controllers/inventoryController")
+
 const app = express()
 
+app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.static("./images"))
+
+app.use("/api/v1/inventory", inventoryController)
