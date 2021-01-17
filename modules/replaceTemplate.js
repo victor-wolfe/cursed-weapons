6ac659722e914dcbd485f7e8ce618db0023b567c
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2021-01-17 11:15:05 -0800

    starting to refactor to express

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,14 +1,21 @@
 const slugify = require("slugify")
+const path = require("path")
+
+let imgDir = path.join(__dirname, "..", "images")
 
 module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%CURSE_LEVEL%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(
+    /{%IMAGE%}/g,
+    `<img src="${imgDir}/${product.id}.png"`
+  )
+  output = output.replace(/{%CURSE_LEVEL%}/g, product.curseLevel)
   output = output.replace(/{%DESCRIPTION%}/g, product.description)
   output = output.replace(/{%ID%}/g, product.id)
+  output = output.replace(/{%PRICE%}/g, product.price)
   output = output.replace(
     /{%SLUG%}/g,
     slugify(product.productName, { lower: true })
   )
+  return output
 }
