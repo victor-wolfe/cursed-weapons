2efa63f9c711c035762b98aca6ab035c9c42fc32
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2021-02-09 07:47:04 -0800

    async wrapper

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,123 +1,80 @@
 const Item = require("../models/itemModel")
 const APIFeatures = require("../utils/APIFeatures")
+const catchAsync = require("../utils/catchAsync")
+const AppError = require("../utils/appError")
 
-// const slugs = storeStock.map((el) => slugify(el.productName, { lower: true }))
+exports.getAllItems = catchAsync(async (req, res) => {
+  const features = new APIFeatures(Item.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
 
-exports.getAllItems = async (req, res) => {
-  try {
-    const features = new APIFeatures(Item.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate()
+  const items = await features.query
+  res.status(200).json({
+    status: "Success",
+    results: items.length,
+    data: {
+      inventory: items,
+    },
+  })
+})
 
-    const items = await features.query
-    res.status(200).json({
-      status: "Success",
-      results: items.length,
-      data: {
-        inventory: items,
-      },
-    })
-  } catch (err) {
-    console.log(err)
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
+exports.getItem = catchAsync(async (req, res) => {
+  const item = await Item.findById(req.params.id)
+  res.status(200).json({
+    status: "Success",
+    data: {
+      inventory: item,
+    },
+  })
+})
 
-exports.getItem = async (req, res) => {
-  try {
-    const item = await Item.findById(req.params.id)
-    res.status(200).json({
-      status: "Success",
-      data: {
-        inventory: item,
-      },
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
+exports.addItem = catchAsync(async (req, res) => {
+  const newItem = await Item.create(req.body)
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: newItem,
+    },
+  })
+})
 
-exports.addItem = async (req, res) => {
-  try {
-    const newItem = await Item.create(req.body)
-    res.status(200).json({
-      status: "success",
-      data: {
-        tour: newItem,
-      },
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
+exports.updateItem = catchAsync(async (req, res) => {
+  const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+  })
+  res.status(201).json({
+    status: "success",
+    data: {
+      inventory: item,
+    },
+  })
+})
 
-exports.updateItem = async (req, res) => {
-  try {
-    const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-    })
-    res.status(201).json({
-      status: "success",
-      data: {
-        inventory: item,
-      },
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
-
-exports.deleteItem = async (req, res) => {
-  try {
-    await Item.findByIdAndDelete(req.params.id)
-    res.status(204).json({
-      status: "success",
-      data: null,
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
+exports.deleteItem = catchAsync(async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id)
+  res.status(204).json({
+    status: "success",
+    data: null,
+  })
+})
 
 // Aggregate Stats
-exports.getItemStats = async (req, res) => {
-  try {
-    const stats = await Item.aggregate([
-      {
-        $group: {
-          _id: { type: "$type" },
-          numItems: { $sum: 1 },
-        },
+exports.getItemStats = catchAsync(async (req, res) => {
+  const stats = await Item.aggregate([
+    {
+      $group: {
+        _id: { type: "$type" },
+        numItems: { $sum: 1 },
       },
-    ])
+    },
+  ])
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        stats,
-      },
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    })
-  }
-}
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats,
+    },
+  })
+})
