86573b6945a011cb2d0d19c2228e61afc63a54fc
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2021-02-28 08:04:35 -0800

    rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
+const rateLimit = require("express-rate-limit")
+
 const AppError = require("./utils/appError")
 const globalErrorHandler = require("./controllers/errorHandler")
 
@@ -7,7 +9,18 @@ const itemRouter = require("./routers/itemRouter")
 const userRouter = require("./routers/userRouter")
 
 const app = express()
-app.use(morgan("dev"))
+
+// Global Middleware
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"))
+
+//allows 100 requests per hour
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. Please try again in an hour.",
+})
+
+app.use("/api", limiter)
 app.use(express.json())
 app.use(express.static("./images"))
 
