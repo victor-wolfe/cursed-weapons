24bef3d9c4fd7fa4e632e27f319cb1f6c9d45346
victor-wolfe <victor.vulpcula+VWGH@gmail.com>
2021-03-06 08:08:34 -0800

    add review schema

---
+++ models/reviewModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const User = require("./userModel")
+const Item = require("./itemModel")
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: [true, "Rating required"],
+      min: 1,
+      max: 5,
+    },
+    review: {
+      type: String,
+      maxlength: [1000, "Calm down there, Tolstoy. 1000 characters max."],
+    },
+    item: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: Item,
+        required: [true, "Review must belong to an item"],
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: User,
+        required: [true, "You must be logged in to review."],
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+const Review = mongoose.model("Review", reviewSchema)
+
+module.exports = Review
